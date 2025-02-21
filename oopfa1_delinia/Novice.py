776209c7db0974c7e126cbe944bc28edb1e4d34d
Filjohn_Delinia <filjohndelinia08@gmail.com>
2025-02-22 00:48:19 +0800

    Add files via upload

---
+++ oopfa1_delinia/Novice.py
@@ -0,0 +1,7 @@
+from Character import Character
+
+class Novice(Character):
+    def basicAttack(self, character):
+        character.reduceHp(self.getDamage())
+        print(f"{self.getUsername()} performed Basic Attack! - {self.getDamage()}")
+    
